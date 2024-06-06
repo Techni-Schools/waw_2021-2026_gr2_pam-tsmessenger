3b63594e356732c47f1e8fdb2e57c24ed1946e61
techniadrian <111449883+techniadrian@users.noreply.github.com>
2024-06-06 14:16:34 +0200

    Fix styles

--- frontend/src/components/ConversationScreen/styles.ts
+++ frontend/src/components/ConversationScreen/styles.ts
@@ -1,7 +1,10 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-container: {}
+  container: {
+    flex: 1,
+    padding: 25,
+  },
 });
 
 export default styles;
--- frontend/src/components/MessageInputBar/styles.ts
+++ frontend/src/components/MessageInputBar/styles.ts
@@ -1,7 +1,11 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-container: {}
+  container: {
+    flexDirection: "row",
+    gap: 10,
+    alignContent: "center",
+  },
 });
 
 export default styles;
